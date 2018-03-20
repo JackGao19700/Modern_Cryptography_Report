46419d0699e0cabbce33dd5ebebf5f25acfc9670
LiuPeng-PC-3 <2224214496@qq.com>
2018-03-20 23:55:37 +0800

    完成了实验报告2：古典加密.

--- #Code/02/1.py
+++ #Code/02/1.py
@@ -1,41 +1,56 @@
-en_alphabet = "abcdefghijklmnopqrstuvwxyz"
-#
-# This function returns true if and only if the character c is an 
-# anphabet character
-#
-def is_alphabetic_char(c):
-	return (c.lower() in en_alphabet)
-#
-# This function converts a single character into its numeric value
-#
-def char_to_num(c):
-	return en_alphabet.index(c.lower())
-#
-# This function returns the character corresponding to x mod 26
 # in the English alphabet
 #
 def num_to_char(x):
-	return en_alphabet[x % 26]
+    return en_alphabet[x % 26]
 
 def CaesarEncrypt(k, plaintext):
-	ciphertext = ""
-	for j in xrange(len(plaintext)):
-		p = plaintext[j]
-		if is_alphabetic_char(p):
-			x = (k + char_to_num(p)) % 26
-			c = num_to_char(x)
-		else:
-			c = p
-		ciphertext += c
-	return ciphertext
+    ciphertext = ""
+    for j in xrange(len(plaintext)):
+        p = plaintext[j]
+        if is_alphabetic_char(p):
+            x = (k + char_to_num(p)) % 26
+            c = num_to_char(x)
+        else:
+            c = p
+        ciphertext += c
+    return ciphertext
 def CaesarDecrypt(k, ciphertext):
-	plaintext = ""
-	for j in xrange(len(ciphertext)):
-		c = ciphertext[j]
-		if is_alphabetic_char(c):
-			x = (char_to_num(c) - k) % 26
-			p = num_to_char(x)
-		else:
-			p = c
-		plaintext += p
-	return plaintext
+    plaintext = ""
+    for j in xrange(len(ciphertext)):
+        c = ciphertext[j]
+        if is_alphabetic_char(c):
+            x = (char_to_num(c) - k) % 26
+            p = num_to_char(x)
+        else:
+            p = c
+        plaintext += p
+    return plaintext
+
+def BruteForceAttack(ciphertext, keyword=None):
+    for k in xrange(26):
+        plaintext = CaesarDecrypt(k, ciphertext)
+        if(None==keyword) or (keyword in plaintext):
+            print "key", k, "decryption", plaintext
+    return
+
+"""------------------have a try------------------"""
+
+
+k = 6; plaintext = "Get me a vanilla ice cream, make it a double." ;
+print  CaesarEncrypt(k, plaintext)
+
+k = 15; plaintext = "I don't much care for Leonard Cohen.";
+print CaesarEncrypt(k, plaintext)
+
+k = 16; plaintext = "I like root beer floats.";
+print CaesarEncrypt(k, plaintext)
+
+"""------------------have a try------------------"""
+
+k = 12; ciphertext = "nduzs ftq buzq oazqe";
+print CaesarDecrypt(k, ciphertext)
+
+k =  3; ciphertext = "fdhvdu qhhgv wr orvh zhljkw.";
+print CaesarDecrypt(k, ciphertext)
+
+k = 20; ciphertext = "ufgihxm uly numnys.";
